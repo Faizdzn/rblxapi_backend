a0d4f0ba7a0cfe68a4fb726839980871843de315
Faizdzn <masterfrostgt@gmail.com>
2024-04-21 19:32:06 +0700

    omaygad

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require("path");
 // const config = JSON.parse(fs.readFileSync(path.resolve(__dirname, "./config.json")));
 const config = process.env;
 const RBXException = require("./lib/error.js");
+const erro = JSON.parse(fs.readFileSync(path.resolve(__dirname, "./lib/json/errorCode.json")));
 
 const express = require("express");
 const cors = require("cors");
@@ -26,19 +27,18 @@ app.use((err, req, res, next) => {
   if(err.json != null) {
     res.status(err.json['error']['status']).json(err.json);
   } else {
-    // const code = "E_SERVER"
-    // const resp =
-    // {
-    //   error: {
-    //     code: code,
-    //     message: config.error['code'][code][0],
-    //     status: config.error['code'][code][1]
-    //   }
-    // };
+    const code = "E_SERVER"
+    const resp =
+    {
+      error: {
+        code: code,
+        message: erro.error['code'][code][0],
+        status: erro.error['code'][code][1]
+      }
+    };
     
-    // console.error(err)
-    // res.status(resp['status']).json(resp);
-    throw new RBXException("E_SERVER");
+    console.error(err)
+    res.status(resp['status']).json(resp);
   }
 })
 
