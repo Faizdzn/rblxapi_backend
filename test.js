a8c6eca91b258cd4aa638e1bdbe62402367b5a60
Faizdzn <masterfrostgt@gmail.com>
2024-04-23 11:43:56 +0700

    e

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ RBX = new RBX('./cdn');
 async function testApi() {
   try {
     const gameDT = await RBX.getGroupDetail(17283984)
-    
+
     console.log(gameDT);
   } catch(err) {
     console.error(err);
